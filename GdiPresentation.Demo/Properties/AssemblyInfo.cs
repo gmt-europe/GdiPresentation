119a343f2e71169dc13b7a78c9d102d981bf33f2
Pieter van Ginkel <pginkel@gmt.nl>
2014-11-01 08:37:52 +0100

    Created version 1.0.2.0.

--- GdiPresentation.Demo/Properties/AssemblyInfo.cs
+++ GdiPresentation.Demo/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("cdc144ec-4d21-4aae-9c07-d399c687a859")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
